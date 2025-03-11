985bab98f9907f946acf2a41b24f520d1c220f9c
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-03-11 19:29:53 -0400

    Create page and components placeholders, and modify App.jsx by adding route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+import NotesPage from "./pages/NotesPage/NotesPage";
+import LinksPage from "./pages/LinksPage/LinksPage";
+import DocumentsPage from "./pages/DocumentsPage/DocumentsPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
-import './App.scss'
+export const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
-
-
   return (
-    <>
-      <div>
-        Welcome to Save-It App
-      </div>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/notes" element={<NotesPage />} />
+        <Route path="/links" element={<LinksPage />} />
+        <Route path="/documents" element={<DocumentsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
