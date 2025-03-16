72e9779006011f41bf41a3f02232d297b2c71921
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-03-16 15:24:29 -0400

    Setup state variables, create use state for fetching data, handle loading and error state

--- src/components/DocumentsList/DocumentsList.jsx
+++ src/components/DocumentsList/DocumentsList.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../../config";
 
-const DocumentsList = () => {
-  return (
-    <div>DocumentsList</div>
-  )
+export default function DocumentList() {
+  const [documents, setDocuments] = useState([]);
+  const [viewAll, setViewAll] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/documents`);
+        setDocuments(response.data);
+      } catch (err) {
+        console.error ("Error fetching documents.")
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchDocuments();
+  }, []);
+
+  if(loading) {
+    return <p>Loading documents...</p>
+  }
+  if (error) {
+    return <p className='error'>{error}</p>
+  }
+
+  // Sort documents by createdAt (newest first)
+  const sortedDocuments = [...documents]
+
 }
-
-export default DocumentsList
