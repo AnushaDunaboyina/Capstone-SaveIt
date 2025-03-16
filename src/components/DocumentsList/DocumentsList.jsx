cfae699355d1ef562b0d1d269f8c26c0b1a97356
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-03-16 16:59:06 -0400

    Try to fix font issue

--- src/components/DocumentsList/DocumentsList.jsx
+++ src/components/DocumentsList/DocumentsList.jsx
@@ -14,22 +14,62 @@ export default function DocumentList() {
         const response = await axios.get(`${API_URL}/api/documents`);
         setDocuments(response.data);
       } catch (err) {
-        console.error ("Error fetching documents.")
+        setError("Failed to fetch documents.");
       } finally {
         setLoading(false);
       }
-    }
+    };
     fetchDocuments();
   }, []);
 
-  if(loading) {
-    return <p>Loading documents...</p>
+  if (loading) {
+    return <p>Loading documents...</p>;
   }
   if (error) {
-    return <p className='error'>{error}</p>
+    return <p className="error">{error}</p>;
   }
 
   // Sort documents by createdAt (newest first)
-  const sortedDocuments = [...documents]
+  const sortedDocuments = [...documents].sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
 
+  // Show the latest 3 documents or all documents based on viewAll state
+  const displayDocuments = viewAll
+    ? sortedDocuments
+    : sortedDocuments.slice(0, 3);
+
+  return (
+    <>
+      <div>
+        <h2>Documents</h2>
+        <div>
+          {!viewAll ? (
+            <button onClick={() => setViewAll(true)}>View All</button>
+          ) : (
+            <button onClick={() => setViewAll(false)}>View Sorted</button>
+          )}
+        </div>
+        <ul>
+          {displayDocuments.map((document) => (
+            <li key={document.id}>
+              <p>Filename:{document.filename}</p>
+              <p>
+                {typeof document.tags === "string"
+                  ? document.tags.split(",").join(", ")
+                  : document.tags}
+              </p>
+              <a
+                href={document.filepath}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Preview Document
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/pages/DocumentsPage/DocumentsPage.jsx
+++ src/pages/DocumentsPage/DocumentsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DocumentUploadForm from "../../components/DocumentUploadForm/DocumentUploadForm";
+import DocumentList from '../../components/DocumentsList/DocumentsList';
 
 const DocumentsPage = () => {
   return (
     <>
-    <DocumentUploadForm/>
+    <DocumentUploadForm />
+    <DocumentList />
     </>
     
   )
